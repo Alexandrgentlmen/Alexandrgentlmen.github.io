745848b35e04dea20256fe59c64361e3e46636d8
Alexandrgentlmen <chegeadaun@yandex.ru>
2019-11-08 17:16:15 +0300

    08.11.19
    js код редакция №1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 document.getElementById('close-board').onmouseover = function(event) {
 	var target = event.target;
-	if (target.className == 'icon-board') {
+	if (target.className == 'price-board') {
 		var s = target.getElementsByClassName('icon-board');
 		closeMenu();
 		s[0].style.display='block';
 	}
 }
-
 document.onmouseover=function(event) {
 	var target = event.target;
 	console.log(event.target);
-	if (target.className!='icon-board' && target.className!='icon-layer'){
+	if (target.className!='price-board' && target.className!='icon-board'){
 		closeMenu();  
 	}
 }
 function closeMenu() {
-	var menu = document.getElementById('close-board');
 	var subm = document.getElementsByClassName('icon-board');
 	for (var i=0; i <subm.length; i++) {
 		subm[i].style.display="none";
 	}
+
 }
